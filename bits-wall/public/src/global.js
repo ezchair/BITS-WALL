e44f99d50acec5e0a7460c59d78a862a12c3e2d2
Hundao <Alchemy_wimp@hotmail.com>
2019-11-16 13:31:37 +0800

    fix dimond naming

--- bits-wall/public/src/global.js
+++ bits-wall/public/src/global.js
@@ -7,7 +7,7 @@ const global = {
 
   bricks: [
     new Brick(0, 100, 100, 1, 1, "RECTANGLE", "ACTIVE", null),
-    new Brick(1, 200, 200, 1, 1, "DIMON", "ACTIVE", null),
+    new Brick(1, 200, 200, 1, 1, "DIMOND", "ACTIVE", null),
     new Brick(2, 300, 300, 1, 1, "STAR", "ACTIVE", null)
   ],
   fBricks: []
