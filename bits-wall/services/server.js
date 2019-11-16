113541818deb598502ebf5cdba44164a4b866410
NoahChou <noah.chou@eztable.com>
2019-11-17 00:43:00 +0800

    Cross domain

--- bits-wall/services/server.js
+++ bits-wall/services/server.js
@@ -8,7 +8,7 @@ const {
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
