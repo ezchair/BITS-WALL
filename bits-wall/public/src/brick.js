02566a8a87e4e429c69cdca984e830fe40aef86a
Hundao <Alchemy_wimp@hotmail.com>
2019-11-16 15:39:08 +0800

    it ok to add brick in admin

--- bits-wall/public/src/brick.js
+++ bits-wall/public/src/brick.js
@@ -1,10 +1,11 @@
 class Brick{
-  constructor(id, x, y, sx, sy, type, active, reward){
+  constructor(id, x, y, sx, sy, angle, type, active, reward){
     this.id = id
     this.x = x
     this.y = y
     this.sx = sx
     this.sy = sy
+    this.angle = angle
     this.type = type
     this.active = active
     this.reward = reward
