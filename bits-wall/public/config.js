a7cdedf814f028d17376df7537677dabf4774b66
nacat <nanahow0911@gmail.com>
2019-11-16 17:23:41 +0800

    add configuration

--- bits-wall/public/config.js
+++ bits-wall/public/config.js
@@ -2,9 +2,34 @@ let token, userId;
 
 const twitch = window.Twitch.ext;
 
+let config = {}
+
 twitch.onContext((context) => {
-  twitch.rig.log('hi Ryan & Noah')
   twitch.rig.log(context);
+
+  for (let i =0; i<3; i++) {
+    setTimeout(() => {
+      twitch.rig.log(i)
+      twitch.rig.log(config)
+    }, 1000)
+  }
+  setTimeout(() => {
+    twitch.rig.log('before')
+    setChannelConfig('1.0', JSON.stringify({'one': 'one'}))
+    twitch.rig.log('after')
+  }, 1000)
+  setTimeout(() => {
+    twitch.rig.log('before')
+    setChannelConfig('2.0', JSON.stringify({'two': 'two'}))
+    twitch.rig.log('after')
+  }, 2000)
+  setTimeout(() => {
+    twitch.rig.log('before')
+    setChannelConfig('3.0', JSON.stringify({'three': 'three'}))
+    twitch.rig.log('after')
+  }, 3000)
+    
+
 });
 
 twitch.onAuthorized((auth) => {
@@ -12,3 +37,17 @@ twitch.onAuthorized((auth) => {
   userId = auth.userId;
 });
 
+twitch.configuration.onChanged( () => {
+  twitch.rig.log('hi')
+  let json = JSON.parse(twitch.configuration.broadcaster.content)
+  twitch.rig.log(json)
+  config = json
+}); 
+
+function setChannelConfig( version, content ) {
+  twitch.configuration.set(
+    "broadcaster",
+    version,
+    content
+  );
+}
