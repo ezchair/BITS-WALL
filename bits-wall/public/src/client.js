52236aec74a2d163ffd58b798f5dbf83dab0c7fd
Hundao <Alchemy_wimp@hotmail.com>
2019-11-17 09:13:29 +0800

    burst is ok

--- bits-wall/public/src/client.js
+++ bits-wall/public/src/client.js
@@ -1,7 +1,8 @@
 const bits = window.Twitch.ext.bits;
-const global = {
+const _GLOBAL = {
   canvas: null,
   ctx: null,
+  fctx: null,
   vw: null,
   vh: null,
   mode: null,
@@ -9,15 +10,22 @@ const global = {
   bricks: [],
   fBricks: [],
   handlingBrick: null,
-  channelId: 43797122
+  channelId: 43797122,
+  burst01 :new Image(),
+  burst02 : new Image(),
+  burst03 : new Image()
 }
+_GLOBAL.burst01.src = './imgs/burst001.png'
+_GLOBAL.burst02.src = './imgs/burst002.png' 
+_GLOBAL.burst03.src = './imgs/burst003.png'
 
+console.log('global', _GLOBAL)
 bits.onTransactionComplete(function (o){
   const productType = o.product.sku;
   console.log(`Transaction ${productType} complete`);
   console.log(o);
-  if(global.handlingBrick){
-    breakBrick(global.handlingBrick.id, global.channelId)
+  if(_GLOBAL.handlingBrick){
+    breakBrick(_GLOBAL.handlingBrick.id, _GLOBAL.channelId)
   }
   // Explore
 });
@@ -31,51 +39,51 @@ window.onload = function(){
   this.console.log("Hello Twitch Hackthon!!")
 
   // init canvas
-  global.canvas = document.createElement('canvas');
-  global.canvas.setAttribute('id', 'canvas')
-  global.ctx = global.canvas.getContext('2d');
+  _GLOBAL.canvas = document.createElement('canvas');
+  _GLOBAL.canvas.setAttribute('id', 'canvas')
+  _GLOBAL.ctx = _GLOBAL.canvas.getContext('2d');
   
   // push canvas to body
-  document.body.appendChild(global.canvas);
-  // console.log(global.canvas)
+  document.body.appendChild(_GLOBAL.canvas);
+  // console.log(_GLOBAL.canvas)
 
   // design canvas weight and height
-  global.vw = 1280;
-  global.vh = 720;
+  _GLOBAL.vw = 1280;
+  _GLOBAL.vh = 720;
 
-  global.canvas.width = global.vw;
-  global.canvas.height = global.vh;
+  _GLOBAL.canvas.width = _GLOBAL.vw;
+  _GLOBAL.canvas.height = _GLOBAL.vh;
 
-  global.fcanvas = new fabric.Canvas('canvas');
-
-  global.fcanvas.on('selection:created', (info)=>{
+  _GLOBAL.fcanvas = new fabric.Canvas('canvas');
+  _GLOBAL.fctx = _GLOBAL.fcanvas.getContext('2d')
+  _GLOBAL.fcanvas.on('selection:created', (info)=>{
     
     if(info.selected.length > 1){
       this.alert('Please select single brick')
-      global.fcanvas.discardActiveObject();
+      _GLOBAL.fcanvas.discardActiveObject();
       return
     }
 
     let target = info.target
-    let brick = global.bricks[target.brickIndex]
-    global.handlingBrick = brick
+    let brick = _GLOBAL.bricks[target.brickIndex]
+    _GLOBAL.handlingBrick = brick
     console.log('brick = ', brick)
     bits.useBits(brick.type);
-    global.fcanvas.discardActiveObject();
+    _GLOBAL.fcanvas.discardActiveObject();
   })
 
   // fake()
 }
 
 const handleReflash = (bricks) =>{
-  global.fBricks = []
-  global.bricks = []
-  global.fcanvas.clear()
+  _GLOBAL.fBricks = []
+  _GLOBAL.bricks = []
+  _GLOBAL.fcanvas.clear()
   
   for(let i in bricks){
     let src = bricks[i]
     let brick = new Brick(src.id, src.x, src.y, src.sx, src.sy, src.angle,src.type, src.active, src.reward)
-    global.bricks.push(brick)
+    _GLOBAL.bricks.push(brick)
 
     if(brick.active){
       console.log('hi')
@@ -88,25 +96,64 @@ const handleReflash = (bricks) =>{
       })
   
       fObj.brickIndex = i
-      global.fBricks.push(fObj)
-      global.fcanvas.add(fObj);
+      _GLOBAL.fBricks.push(fObj)
+      _GLOBAL.fcanvas.add(fObj);
     }
 
   }
 }
 
-// let fake = () =>{
-//   handleReflash([
-//       new Brick(0, 100, 100, 1, 1, 0, "RECTANGLE", true, null),
-//       new Brick(1, 200, 200, 1, 1, 0, "DIMOND", true, null),
-//       new Brick(2, 300, 300, 1, 1, 0, "STAR", true, null)
-//   ])
-// }
+const drawBurst = (brick) => {
+  const random_number = Math.random()
+  let img = null
+  if(random_number > 0.5){
+    img = _GLOBAL.burst01
+  }
+  else{
+    img = _GLOBAL.burst02
+  }
 
-// let fake2 = () =>{
-//   handleReflash([
-//       new Brick(0, 100, 150, 1, 1, 45, "RECTANGLE", true, null),
-//       new Brick(1, 200, 250, 1, 1, 45, "DIMOND", true, null),
-//       new Brick(2, 300, 350, 1, 1, 45, "STAR", true, null)
-//   ])
-// }
+  // let img = _GLOBAL.burst03
+  let speed = 80
+
+  // draw block first
+  let fObj = new fabric.Image(BRICK_INFO[brick.type].image,{
+    left: brick.x,
+    top: brick.y,
+    scaleX: brick.sy,
+    scaleY: brick.sx,
+    angle: brick.angle
+  })
+  _GLOBAL.fcanvas.add(fObj);
+
+  const boom = {
+    x: brick.x +  ( (120 * brick.sx) - 96 ), // * Math.sin(brick.angle * Math.PI / 180),
+    y: brick.y +  ( (70 * brick.sy)  - 96 ) //* Math.cos(brick.angle * Math.PI / 180)
+  }
+
+  for(let i = 0; i < 25; i++){
+    setTimeout(()=>{
+      _GLOBAL.fcanvas.renderAll()
+      _GLOBAL.fctx.drawImage(
+        img,
+        i * 192,
+        0,
+        192,192,
+        boom.x, boom.y,
+        192,192
+      )
+      // _GLOBAL.fcanvas.clear()
+
+      // _GLOBAL.fcanvas.renderAll()
+    }, i * speed)
+  }
+
+  setTimeout(()=>{
+    _GLOBAL.fcanvas.remove(fObj)
+  }, 15 * speed)
+
+  setTimeout(()=>{
+    _GLOBAL.fcanvas.renderAll()
+  }, 25 * speed)
+  
+}
